1451cbbb792acc95dd30a6309fcfeebcc9d3c0dc
SmolenskiyAV <JoJo_666@list.ru>
2022-02-23 21:54:14 +0700

    версия 1.0

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,29 @@
-import sum from '../basic';
+import sum, {checkNumberInput, arrOfOdjets, findBy} from '../basic';
 
 test('should sum', () => {
   const result = sum([1, 2, 3]);
 
   expect(result).toBe(6);
+});
+
+// проверка преобразования функцией checkNumberInput валидных значений 
+test('shoud indicate number', () => {
+  const result = typeof checkNumberInput(' 334 ');
+  expect(result).toBe('number');
+});
+
+// проверка преобразования функцией checkNumberInput невалидных значений 
+test('shoud indicate NaN', () => {
+  const result = checkNumberInput(' 3j34 ');
+  expect(result).toBe(NaN);
+});
+
+// проверка поиска функцией findBy объекта по известному свойству и его значению в исходном массиве объектов arrOfOdjets
+test('shoud indicate: MATCH! ', () => {
+  const result = undefined;
+  if (findBy('name', 'урон', arrOfOdjets) === {
+    name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса'
+    } ) 
+    result = 'MATCH!';
+  expect(result).toBe('MATCH!');
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -5,3 +5,37 @@ export default function sum(items) {
   }
   return result;
 }
+
+export function checkNumberInput(value) { // функция преобразования строки в число
+
+    try {
+
+      if ((value === true) || (value === false) || (value === null)) value = undefined;  // если параметр булевый или null - не преобразовываем в числа (выдаём ошибку)
+
+      return Number(value); // преобразование входного параметра в число
+
+    }
+    catch (e) {
+      console.log('Ошибка: ', e);
+    }
+}
+
+
+export const arrOfOdjets = [ // исходный массив объектов
+  {name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями'},
+  {name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием'},
+  {name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса'},
+];
+
+export function findBy(qualityName, valueOfQaulityName, sourceArray) { // функция поиска обекта в исходном массиве по свойству объекта и значению этого свойства
+
+  for (let i = 0; i < sourceArray.length; i++) {  // перебор объектов исходного массива
+    let tempObject = undefined;
+    for (let quality in quality.sourceArray[i]) {  // перебор свойств каждого объекта в исходном массиве
+      if ((quality === qualityName) && (quality.sourceArray[i] === valueOfQaulityName)) { // если есть совпадение по свойству и его значению (подразумевается уникальность каж. свойства обекта в массиве)
+        tempObject = quality.sourceArray[i]; // передаём найденный объект в возврат функции
+      }
+    }
+  }
+  return tempObject;
+}
